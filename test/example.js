298d6f4586ce674778a407f772cdaf93baf5f8fe
Eddie McWhirter <emcwhirter@usgs.gov>
2015-01-26 12:47:31 -0700

    Change all tabs to 2 spaces.

--- test/example.js
+++ test/example.js
@@ -1,199 +1,199 @@
 /* global require */
 require.config({
-	baseUrl: '..',
-	paths: {
-		'mvc': '/hazdev-webutils/src/mvc',
+  baseUrl: '..',
+  paths: {
+    'mvc': '/hazdev-webutils/src/mvc',
     'util': '/hazdev-webutils/src/util'
-	}
+  }
 });
 
 require([
-	'QuestionView'
+  'QuestionView'
 ], function (
-	QuestionView
+  QuestionView
 ) {
-	'use strict';
+  'use strict';
 
-	// Callback function for question change events
-	var onQuestionChange = function (question) {
-		console.log('question has changed');
-		console.log(question.getAnswers());
-	};
+  // Callback function for question change events
+  var onQuestionChange = function (question) {
+    console.log('question has changed');
+    console.log(question.getAnswers());
+  };
 
 /*
    // ----------------------------------------------------------------------
    // Example of all values with comments
    // ----------------------------------------------------------------------
-	new QuestionView({
-		el: document.querySelector('.question1'), // Match the class in your html
-		label:'This is a question?',              // Ask your question
-		multiSelect:false,               // Allow multiple answers or only one?
-		selectedAnswer:'second-answer',  // Use a string for single default answer
-		//selectedAnswer:[               // Use an array to select multiple defaults
-		//	'second-answer', // These strings need to match one of the answer:values
-		//	'third-answer'
-		//],
-		answers:[                        // Array of answer options objects
-			{
-				value:'first-answer',        // 'Value' for the input element
-				label:'Answer 1'             // The human-friendly answer to show
-				//otherValue:'how-high',     // Extra value when this answer is selected
-				//otherLabel:'How high?'     // Default answer, replaced by user input
-			},
-			{
-				value:'second-answer',
-				label:'Answer 2'
-			},
-			{      // As many answers as you need for your question, each in an object
-				value:'third-answer',
-				label:'Answer 3'
-			}
-		]
-	});
+  new QuestionView({
+    el: document.querySelector('.question1'), // Match the class in your html
+    label:'This is a question?',              // Ask your question
+    multiSelect:false,               // Allow multiple answers or only one?
+    selectedAnswer:'second-answer',  // Use a string for single default answer
+    //selectedAnswer:[               // Use an array to select multiple defaults
+    //  'second-answer', // These strings need to match one of the answer:values
+    //  'third-answer'
+    //],
+    answers:[                        // Array of answer options objects
+      {
+        value:'first-answer',        // 'Value' for the input element
+        label:'Answer 1'             // The human-friendly answer to show
+        //otherValue:'how-high',     // Extra value when this answer is selected
+        //otherLabel:'How high?'     // Default answer, replaced by user input
+      },
+      {
+        value:'second-answer',
+        label:'Answer 2'
+      },
+      {      // As many answers as you need for your question, each in an object
+        value:'third-answer',
+        label:'Answer 3'
+      }
+    ]
+  });
 */
 
-	new QuestionView({
-		el: document.querySelector('.question1'),
-		label:'This is a question?',
-		multiSelect:false,
-		selectedAnswer:'second-answer',
-		answers:[
-			{
-				value:'first-answer',
-				label:'Answer 1'
-			},
-			{
-				value:'second-answer',
-				label:'Answer 2'
-			},
-			{
-				value:'third-answer',
-				label:'Answer 3'
-			}
-		]
-	});
+  new QuestionView({
+    el: document.querySelector('.question1'),
+    label:'This is a question?',
+    multiSelect:false,
+    selectedAnswer:'second-answer',
+    answers:[
+      {
+        value:'first-answer',
+        label:'Answer 1'
+      },
+      {
+        value:'second-answer',
+        label:'Answer 2'
+      },
+      {
+        value:'third-answer',
+        label:'Answer 3'
+      }
+    ]
+  });
 
-	// This question is bound anonymously to the callback function
-	new QuestionView({
-		el: document.querySelector('.question2'),
-		label:'This IS a question!',
-		multiSelect:true,
-		answers:[
-			{
-				value:'first-answer',
-				label:'My answer'
-			},
-			{
-				value:'second-answer',
-				label:'Your answer'
-			},
-			{
-				value:'third-answer',
-				label:'Their answer'
-			}
-		]
-	}).on('change', onQuestionChange);
+  // This question is bound anonymously to the callback function
+  new QuestionView({
+    el: document.querySelector('.question2'),
+    label:'This IS a question!',
+    multiSelect:true,
+    answers:[
+      {
+        value:'first-answer',
+        label:'My answer'
+      },
+      {
+        value:'second-answer',
+        label:'Your answer'
+      },
+      {
+        value:'third-answer',
+        label:'Their answer'
+      }
+    ]
+  }).on('change', onQuestionChange);
 
-	new QuestionView({
-		el: document.querySelector('.question3'),
-		label:'This one uses an "other" box',
-		multiSelect:false,
-		answers:[
-			{
-				value:'first-answer',
-				label:'Some answer'
-			},
-			{
-				value:'second-answer',
-				label:'Another answer',
-				otherValue:'',
-				otherLabel:'Other'
-			}
-		]
-	});
+  new QuestionView({
+    el: document.querySelector('.question3'),
+    label:'This one uses an "other" box',
+    multiSelect:false,
+    answers:[
+      {
+        value:'first-answer',
+        label:'Some answer'
+      },
+      {
+        value:'second-answer',
+        label:'Another answer',
+        otherValue:'',
+        otherLabel:'Other'
+      }
+    ]
+  });
 
-	var question4 = new QuestionView({
-		el: document.querySelector('.question4'),
-		label:'These ones are in a div',
-		multiSelect:true,
-		selectedAnswer:[
-			'second-answer',
-			'fourth-answer'
-		],
-		answers:[
-			{
-				value:'first-answer',
-				label:'#1'
-			},
-			{
-				value:'second-answer',
-				label:'#2'
-			},
-			{
-				value:'third-answer',
-				label:'#3'
-			},
-			{
-				value:'fourth-answer',
-				label:'#4'
-			},
-			{
-				value:'fifth-answer',
-				label:'#5'
-			}
-		]
-	});
+  var question4 = new QuestionView({
+    el: document.querySelector('.question4'),
+    label:'These ones are in a div',
+    multiSelect:true,
+    selectedAnswer:[
+      'second-answer',
+      'fourth-answer'
+    ],
+    answers:[
+      {
+        value:'first-answer',
+        label:'#1'
+      },
+      {
+        value:'second-answer',
+        label:'#2'
+      },
+      {
+        value:'third-answer',
+        label:'#3'
+      },
+      {
+        value:'fourth-answer',
+        label:'#4'
+      },
+      {
+        value:'fifth-answer',
+        label:'#5'
+      }
+    ]
+  });
 
-	// Set the question to a variable to use in the callback function
-	var question5 = new QuestionView({
-		el: document.querySelector('.question5'),
-		label:'One last question',
-		multiSelect:true,
-		selectedAnswer:[
-			'answer-high',
-			'answer-low'
-		],
-		answers:[
-			{
-				value:'answer-high',
-				label:'High',
-				otherValue:'how-high',
-				otherLabel:'How high?'
-			},
-			{
-				value:'answer-low',
-				label:'Low',
-				otherValue:'how-low',
-				otherLabel:'How low?'
-			},
-			{
-				value:'answer-up',
-				label:'Up',
-				otherValue:'up-confirm',
-				otherLabel:'Realy?'
-			},
-			{
-				value:'answer-down',
-				label:'Down',
-				otherValue:'down-confirm',
-				otherLabel:'Are you sure?'
-			},
-			{
-				value:'answer-left',
-				label:'Left',
-				otherValue:'left',
-				otherLabel:'My left or yours?'
-			},
-			{
-				value:'answer-right',
-				label:'Right',
-				otherValue:'right',
-				otherLabel:'Yesterday?'
-			}
-		]
-	});
+  // Set the question to a variable to use in the callback function
+  var question5 = new QuestionView({
+    el: document.querySelector('.question5'),
+    label:'One last question',
+    multiSelect:true,
+    selectedAnswer:[
+      'answer-high',
+      'answer-low'
+    ],
+    answers:[
+      {
+        value:'answer-high',
+        label:'High',
+        otherValue:'how-high',
+        otherLabel:'How high?'
+      },
+      {
+        value:'answer-low',
+        label:'Low',
+        otherValue:'how-low',
+        otherLabel:'How low?'
+      },
+      {
+        value:'answer-up',
+        label:'Up',
+        otherValue:'up-confirm',
+        otherLabel:'Realy?'
+      },
+      {
+        value:'answer-down',
+        label:'Down',
+        otherValue:'down-confirm',
+        otherLabel:'Are you sure?'
+      },
+      {
+        value:'answer-left',
+        label:'Left',
+        otherValue:'left',
+        otherLabel:'My left or yours?'
+      },
+      {
+        value:'answer-right',
+        label:'Right',
+        otherValue:'right',
+        otherLabel:'Yesterday?'
+      }
+    ]
+  });
 
-	// Bind the callback function to the change event
-	question5.on('change', onQuestionChange);
-	question4.on('change', onQuestionChange);
+  // Bind the callback function to the change event
+  question5.on('change', onQuestionChange);
+  question4.on('change', onQuestionChange);
 });
--- test/index.js
+++ test/index.js
@@ -6,71 +6,71 @@
 //     https://developer.mozilla.org
 //         /en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
 if (!Function.prototype.bind) {
-	Function.prototype.bind = function (oThis) {
-		if (typeof this !== 'function') {
-			// closest thing possible to the ECMAScript 5 internal IsCallable
-			throw new TypeError('object to be bound is not callable');
-		}
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5 internal IsCallable
+      throw new TypeError('object to be bound is not callable');
+    }
 
-		var aArgs = Array.prototype.slice.call(arguments, 1),
-		    fToBind = this,
-		    NOP = function () {},
-		    fBound;
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        NOP = function () {},
+        fBound;
 
-		fBound = function () {
-			return fToBind.apply(
-					(this instanceof NOP && oThis ? this : oThis),
-					aArgs.concat(Array.prototype.slice.call(arguments)));
-		};
+    fBound = function () {
+      return fToBind.apply(
+          (this instanceof NOP && oThis ? this : oThis),
+          aArgs.concat(Array.prototype.slice.call(arguments)));
+    };
 
-		NOP.prototype = this.prototype;
-		fBound.prototype = new NOP();
+    NOP.prototype = this.prototype;
+    fBound.prototype = new NOP();
 
-		return fBound;
-	};
+    return fBound;
+  };
 }
 
 
 require.config({
-	baseUrl: '..',
-	paths: {
-		mocha: 'mocha/mocha',
-		chai: 'chai/chai',
-		sinon: 'sinon/pkg/sinon',
-		'mvc': '/hazdev-webutils/src/mvc',
+  baseUrl: '..',
+  paths: {
+    mocha: 'mocha/mocha',
+    chai: 'chai/chai',
+    sinon: 'sinon/pkg/sinon',
+    'mvc': '/hazdev-webutils/src/mvc',
     'util': '/hazdev-webutils/src/util'
-	},
-	shim: {
-		mocha: {
-			exports: 'mocha'
-		},
-		chai: {
-			deps: ['mocha'],
-			exports: 'chai'
-		},
-		sinon: {
-			exports: 'sinon'
-		}
-	}
+  },
+  shim: {
+    mocha: {
+      exports: 'mocha'
+    },
+    chai: {
+      deps: ['mocha'],
+      exports: 'chai'
+    },
+    sinon: {
+      exports: 'sinon'
+    }
+  }
 });
 
 require([
-	'mocha',
+  'mocha',
 ], function (
-	mocha
+  mocha
 ) {
-	'use strict';
+  'use strict';
 
-	mocha.setup('bdd');
+  mocha.setup('bdd');
 
-	// Add each test class here as they are implemented
-	require([
-		'spec/QuestionViewTest'
-	], function () {
-		if (window.mochaPhantomJS) {
-			window.mochaPhantomJS.run();
-		} else {
-			mocha.run();
-		}
-	});
+  // Add each test class here as they are implemented
+  require([
+    'spec/QuestionViewTest'
+  ], function () {
+    if (window.mochaPhantomJS) {
+      window.mochaPhantomJS.run();
+    } else {
+      mocha.run();
+    }
+  });
 });

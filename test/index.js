2760e94ed08e0e2e890429d76e2df7ac3420b62c
Eddie McWhirter <emcwhirter@usgs.gov>
2015-01-27 14:57:14 -0700

    Replace polyfill with newer one and browserify test index.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-'use strict';
+/* global mocha */
 
 // PhantomJS is missing native bind support,
 //     https://github.com/ariya/phantomjs/issues/10522
@@ -7,6 +7,7 @@
 //         /en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
 if (!Function.prototype.bind) {
   Function.prototype.bind = function (oThis) {
+  'use strict';
     if (typeof this !== 'function') {
       // closest thing possible to the ECMAScript 5 internal IsCallable
       throw new TypeError('object to be bound is not callable');
@@ -14,60 +15,34 @@ if (!Function.prototype.bind) {
 
     var aArgs = Array.prototype.slice.call(arguments, 1),
         fToBind = this,
-        NOP = function () {},
+        FNOP = function () {},
         fBound;
 
     fBound = function () {
       return fToBind.apply(
-          (this instanceof NOP && oThis ? this : oThis),
+          (this instanceof FNOP && oThis ? this : oThis),
           aArgs.concat(Array.prototype.slice.call(arguments)));
     };
 
-    NOP.prototype = this.prototype;
-    fBound.prototype = new NOP();
+    FNOP.prototype = this.prototype;
+    fBound.prototype = new FNOP();
 
     return fBound;
   };
 }
 
-var config = {
-  chai: require('chai/chai'),
-  connect: require('../gruntconfig/connect'),
-  jshint: require('../gruntconfig/jshint'),
-  mocha_phantomjs: require('../gruntconfig/mocha_phantomjs'),
-  mvc: require('mvc'),
-  sinon: require('sinon/pkg/sinon'),
-  util: require('util'),
-  watch: require('../gruntconfig/watch'),
-
-  tasks: [
-    'grunt-contrib-connect',
-    'grunt-contrib-jshint',
-    'grunt-mocha-phantomjs',
-    'grunt-contrib-watch'
-  ]
-};
-
-
-require([
-  'mocha',
-], function (
-  mocha
-) {
+(function () {
   'use strict';
 
-  mocha.setup('bdd');
+  mocha.ui('bdd');
+  mocha.reporter('html');
 
   // Add each test class here as they are implemented
-  require([
-    'spec/QuestionViewTest'
-  ], function () {
-    if (window.mochaPhantomJS) {
-      window.mochaPhantomJS.run();
-    } else {
-      mocha.run();
-    }
-  });
-});
+  require('./spec/QuestionViewTest');
 
-module.exports = config;
+  if (window.mochaPhantomJS) {
+      window.mochaPhantomJS.run();
+  } else {
+    mocha.run();
+  }
+})(this);

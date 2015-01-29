353a5eaa887e5f0fd222050410b573f993d5097b
Eddie McWhirter <emcwhirter@usgs.gov>
2015-01-29 09:59:34 -0700

    Add test and dist options in connect.js.

--- gruntconfig/connect.js
+++ gruntconfig/connect.js
@@ -9,12 +9,32 @@ var connect = {
   dev: {
     options: {
       base: [
-        config.build + '/' + config.test,
+        config.build + '/' + config.example,
         config.build + '/' + config.src,
       ],
       livereload: true,
       open: 'https://localhost:8000',
       port: 8000
+    }
+  },
+  test: {
+    options: {
+      base: [
+        config.build + '/' + config.test,
+        config.build + '/' + config.src,
+        'node_modules'
+      ],
+      port: 8001
+    }
+  },
+  dist: {
+    options: {
+      base: [
+        config.dist
+      ],
+      keepalive: true,
+      open: 'http://localhost:8002/',
+      port: 8002
     }
   }
 };

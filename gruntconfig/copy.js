6c4abc52ba5012ac4e186d8d1a4011554e4c525a
Eddie McWhirter <emcwhirter@usgs.gov>
2015-01-29 15:46:41 -0700

    Expand src array to be easier to read.

--- gruntconfig/copy.js
+++ gruntconfig/copy.js
@@ -6,21 +6,30 @@ var copy = {
   example: {
     expand: true,
     cwd: config.example,
-    src: ['*.html', '*.js'],
+    src: [
+      '*.html',
+      '*.js'
+    ],
     dest: config.build + '/' + config.example
   },
 
   test: {
     expand: true,
     cwd: config.test,
-    src: ['*.html', '*.js'],
+    src: [
+      '*.html',
+      '*.js'
+    ],
     dest: config.build + '/' + config.test
   },
 
   mocha: {
     expand: true,
     cwd: 'node_modules',
-    src: ['mocha/mocha.js', 'mocha/mocha.css'],
+    src: [
+      'mocha/mocha.js',
+      'mocha/mocha.css'
+    ],
     dest: config.build + '/' + config.test
   }
 };

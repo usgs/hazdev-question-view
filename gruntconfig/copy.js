58d6522281f645d25924301600e3138c2ac0566a
Eddie McWhirter <emcwhirter@usgs.gov>
2015-01-30 11:47:52 -0700

    Add dist section to copy.

--- gruntconfig/copy.js
+++ gruntconfig/copy.js
@@ -3,6 +3,15 @@
 var config = require('./config');
 
 var copy = {
+  dist: {
+    expand: true,
+    cwd: config.test,
+    src: [
+      'example.html'
+    ],
+    dest: config.dist
+  },
+
   example: {
     expand: true,
     cwd: config.test,

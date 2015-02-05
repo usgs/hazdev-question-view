06dbc31d6edb3a26bb9dbb3a19e741dd256905c4
Eddie McWhirter <emcwhirter@usgs.gov>
2015-02-05 13:22:50 -0700

    Don't copy example.html to dist.

--- gruntconfig/copy.js
+++ gruntconfig/copy.js
@@ -3,15 +3,6 @@
 var config = require('./config');
 
 var copy = {
-  dist: {
-    expand: true,
-    cwd: config.test,
-    src: [
-      'example.html'
-    ],
-    dest: config.dist
-  },
-
   build: {
     expand: true,
     cwd: config.test,
